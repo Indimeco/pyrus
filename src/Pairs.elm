a0a933713cc123df9624c62060770aed9ef4e631
Indimeco <18373729+Indimeco@users.noreply.github.com>
2022-10-08 09:58:05 +1100

    prototype list randomisation

--- src/Pairs.elm
+++ src/Pairs.elm
@@ -1,4 +1,7 @@
-module Pairs exposing (Pairing(..), fromString, toPairs)
+module Pairs exposing (Pairing(..), fromString, shuffle, toPairs)
+
+import Array
+import Random
 
 
 type Pairing
@@ -25,3 +28,44 @@ toPairs input =
 fromString : String -> List Pairing
 fromString =
     String.split "\n" >> List.map String.trim >> List.filter (not << String.isEmpty) >> toPairs
+
+
+shuffle : Random.Seed -> List a -> List a
+shuffle seed list =
+    shuffleListHelper seed list []
+
+
+shuffleListHelper : Random.Seed -> List a -> List a -> List a
+shuffleListHelper seed source result =
+    -- based on https://stackoverflow.com/questions/42207900/how-to-shuffle-a-list-in-elm
+    if List.isEmpty source then
+        result
+
+    else
+        let
+            indexGenerator =
+                Random.int 0 (List.length source - 1)
+
+            ( index, nextSeed ) =
+                Random.step indexGenerator seed
+
+            valAtIndex =
+                Array.fromList source |> Array.get index
+
+            firstSlice =
+                Array.fromList source |> Array.slice 0 index
+
+            secondSlice =
+                Array.fromList source |> Array.slice (index + 1) (List.length source + 1)
+
+            sourceWithoutIndex =
+                Array.toList firstSlice ++ Array.toList secondSlice
+        in
+        case valAtIndex of
+            Just val ->
+                shuffleListHelper nextSeed sourceWithoutIndex (val :: result)
+
+            Nothing ->
+                -- we somehow generated an index outside of the list
+                -- we just return the unrandomised list
+                source
--- tests/PairsTest.elm
+++ tests/PairsTest.elm
@@ -3,6 +3,7 @@ module PairsTest exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Pairs exposing (Pairing(..), toPairs)
+import Random
 import Test exposing (..)
 
 
@@ -40,5 +41,15 @@ suite =
             , test "makes pairs given a string with bad formatting" <|
                 \_ ->
                     Expect.equal [ Pair "Jack" "Jill", Pair "Sam" "Frodo", Unmatched "Gandalf" ] <| Pairs.fromString "Jack\n\n\nJill\nSam \n    Frodo\nGandalf     "
+            ]
+        , describe "shuffle"
+            [ test "does nothing to an empty list" <|
+                \_ -> Expect.equal [] <| Pairs.shuffle (Random.initialSeed 1) []
+            , test "does nothing to single value list" <|
+                \_ -> Expect.equal [ "duck" ] <| Pairs.shuffle (Random.initialSeed 1) [ "duck" ]
+            , test "randomises a small list in one order" <|
+                \_ -> Expect.equal [ 1, 2 ] <| Pairs.shuffle (Random.initialSeed 1) [ 1, 2 ]
+            , test "randomises a small list in a different order" <|
+                \_ -> Expect.equal [ 2, 1 ] <| Pairs.shuffle (Random.initialSeed 2) [ 1, 2 ]
             ]
         ]
